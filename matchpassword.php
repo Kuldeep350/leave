d61be8e389f6cf85353fc536014db839894e066b
Kuldeep350 <855deepsingh@gmail.com>
2018-09-10 15:47:09 +0530

    hello

--- matchpassword.php
+++ matchpassword.php
@@ -1,6 +1,39 @@
 <html>
-
 <head>
+<style>
+ h1, p{
+  margin-left:15%;
+ }
+ body{
+  background-image:url("ks.jpg");
+  }
+#a{
+padding:50px;
+align:center;
+margin-left:20%;
+margin-top:35px;
+background: -webkit-gradient(linear, left top, left bottom, color-stop(100%,rgba(0,0,0,0.30)), color-stop(100%,rgba(0,0,0,0.30))); border-radius:10px;
+width:50%;
+color:lightblue;
+}
+form{
+  margin-left:20%;
+}
+.btn{
+border-radius:5px;
+margin-top:5%;
+}
+ textarea
+ { margin-left: -3px;
+ border-radius:6px;
+  }
+ footer {
+     position: relative;
+     margin-top:70em;
+        width: 100%;
+ }
+</style>
+
     <title> Password and Confirm Password Validation Using Jquery </title>
     <script type="text/javascript" src="http://www.technicalkeeda.com/js/javascripts/plugin/jquery.js"></script>
     <script type="text/javascript" src="http://www.technicalkeeda.com/js/javascripts/plugin/jquery.validate.js"></script>
